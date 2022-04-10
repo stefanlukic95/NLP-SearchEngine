9a992de8b6639b28966091b6e66166e684737c76
Stefan Lukic <stefan.lukic@yahoo.com>
2022-04-10 16:10:22 +0200

    final

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def main():
     # load and clean data
     load_corpus = data_preprocessing.load_data(data_path)
     clean_corpus = data_preprocessing.clean_data(load_corpus)
-    clean_corpus = clean_corpus[:10]
+    clean_corpus = clean_corpus[:1000]
     print(clean_corpus[:5])
 
     vectorizer = TfidVectorize(clean_corpus)
